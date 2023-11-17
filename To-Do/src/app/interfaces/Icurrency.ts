c3cf5de04148a50c9f1c5309c8777117932d7f2e
Sergio Sanjurjo <86019664+SergioPatricioSanjurjo@users.noreply.github.com>
2023-11-17 20:10:55 -0300

    Delete To-Do directory
    dupped

--- To-Do/src/app/interfaces/Icurrency.ts
+++
@@ -1,8 +0,0 @@
-export interface Icurrency{
-    casa:string,
-    compra:number,
-    fechaActualizacion: string,
-    moneda:string,
-    nombre:string,
-    venta:number      
-}                    
